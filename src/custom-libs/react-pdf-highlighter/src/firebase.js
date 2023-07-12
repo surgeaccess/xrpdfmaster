3b848dee08cdb175a9f760e1f4feef2ea495e0d5
surgeaccess <35838481+surgeaccess@users.noreply.github.com>
2023-07-12 10:18:29 +0100

    Update firebase.js

--- src/custom-libs/react-pdf-highlighter/src/firebase.js
+++ src/custom-libs/react-pdf-highlighter/src/firebase.js
@@ -4,8 +4,8 @@ import 'firebase/database';
 const firebaseConfig = {
   apiKey: 'YOUR_API_KEY',
   authDomain: 'YOUR_AUTH_DOMAIN',
-  databaseURL: 'https://xrpdf-3e5d1-default-rtdb.firebaseio.com',
-  projectId: 'xrpdf-3e5d1',
+  databaseURL: 'https://default-rtdb.firebaseio.com',
+  projectId: '',
   storageBucket: 'YOUR_STORAGE_BUCKET',
   messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
   appId: 'YOUR_APP_ID',
